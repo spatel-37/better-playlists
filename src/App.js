1cdfe5fb74d0e2ce7e64b55335a8b7f03014336f
Sanjay Patel <spatel37@users.noreply.github.com>
2020-02-24 23:03:43 +0000

    dynamic listing of fake server playlist data

--- src/App.js
+++ src/App.js
@@ -11,31 +11,31 @@ let fakeServerData = {
       {
         name: 'My favourites',
         songs: [
-          {name: 'Juicy', duration: 1233},
-          {name: 'Drop it Like its Hot', duration: 34234},
-          {name: 'One More Chance', duration: 23423}
+          { name: 'Juicy', duration: 1233 },
+          { name: 'Drop it Like its Hot', duration: 34234 },
+          { name: 'One More Chance', duration: 23423 }
         ]
       },
       {
-        name: 'My favourites1',
-        songs: [          
-        {name: 'Juicy', duration: 1233},
-        {name: 'Drop it Like its Hot', duration: 34234},
-        {name: 'One More Chance', duration: 23423}]
+        name: 'Gym Music',
+        songs: [
+          { name: 'Put your back into it', duration: 1233 },
+          { name: 'Bille Jean', duration: 34234 },
+          { name: 'Candy on the dancefloor', duration: 23423 }]
       },
       {
-        name: 'My favourites2',
-        songs: [          
-        {name: 'Juicy', duration: 1233},
-        {name: 'Drop it Like its Hot', duration: 34234},
-        {name: 'One More Chance', duration: 23423}]
+        name: 'Chill Out',
+        songs: [
+          { name: 'Halcyon on and on', duration: 1233 },
+          { name: 'Barbers Adagio for strings', duration: 34234 },
+          { name: 'Seven Colours', duration: 23423 }]
       },
       {
-        name: 'My favourites3',
-        songs: [          
-        {name: 'Juicy', duration: 1233},
-        {name: 'Drop it Like its Hot', duration: 34234},
-        {name: 'One More Chance', duration: 23423}]
+        name: 'House',
+        songs: [
+          { name: 'Work', duration: 1233 },
+          { name: '24', duration: 34234 },
+          { name: 'For an Angel', duration: 23423 }]
       }
     ]
   }
@@ -55,13 +55,13 @@ class HoursCounter extends Component {
   render() {
     let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
       return songs.concat(eachPlaylist.songs)
-    },[])
+    }, [])
     let totalDuration = allSongs.reduce((sum, eachSong) => {
       return sum + eachSong.duration
     }, 0)
     return (
       <div style={{ ...defaultStyle, width: "40%", display: 'inline-block' }}>
-        <h2>{Math.round(totalDuration/60)} hours</h2>
+        <h2>{Math.round(totalDuration / 60)} hours</h2>
       </div>
     );
   }
@@ -82,11 +82,17 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render() {
+    let playlist = this.props.playlist
     return (
       <div style={{ ...defaultStyle, display: 'inline-block', width: '20%' }}>
         <img />
-        <h3>Playlist Name</h3>
-        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+        <h3>{playlist.name}</h3>
+        <ul>
+          {playlist.songs.map(song => 
+            <li>{song.name}</li>
+          )}
+        </ul>
+
       </div>
     );
   }
@@ -112,12 +118,11 @@ class App extends Component {
               {this.state.serverData.user.name}'s Playlists
           </h1>
             <PlaylistCounter playlists={this.state.serverData.user.playlists} />
-            <HoursCounter playlists={this.state.serverData.user.playlists}/>
+            <HoursCounter playlists={this.state.serverData.user.playlists} />
             <Filter />
-            <Playlist />
-            <Playlist />
-            <Playlist />
-            <Playlist />
+            {this.state.serverData.user.playlists.map(playlist =>
+              <Playlist playlist={playlist} />
+            )}
           </div> : <h1 style={defaultStyle}>Loading...</h1>
         }
       </div>
