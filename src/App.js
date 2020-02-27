3d3251bd3ea59a81c4efb0c326d3e9428efaf417
Sanjay Patel <spatel37@users.noreply.github.com>
2020-02-27 01:36:12 +0000

    Added an if statement to check for an access token. Also added conditions for the sign in button to check if its localhost or production backend.

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class Playlist extends Component {
     let playlist = this.props.playlist
     return (
       <div style={{ ...defaultStyle, display: 'inline-block', width: '20%' }}>
-        <img src={playlist.imageUrl} style={{width: '60px'}}/>
+        <img src={playlist.imageUrl} style={{ width: '60px' }} />
         <h3>{playlist.name}</h3>
         <ul>
           {playlist.songs.map(song =>
@@ -90,15 +90,17 @@ class App extends Component {
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
 
     fetch('https://api.spotify.com/v1/me', {
-      headers: {'Authorization': 'Bearer ' + accessToken}
+      headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(response => response.json())
-    .then(data => this.setState({
-      user: {
-        name: data.display_name
-      }
-    }))
+      .then(data => this.setState({
+        user: {
+          name: data.display_name
+        }
+      }))
 
     fetch('https://api.spotify.com/v1/me/playlists', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
@@ -136,8 +138,12 @@ class App extends Component {
             {playlistToRender.map(playlist =>
               <Playlist playlist={playlist} />
             )}
-          </div> : <button onClick={() => window.location = 'http://localhost:8888/login'}
-            style={{ padding: '20px', 'font-size': '50px', 'margin-top': '20px' }}>Sign in with Spotify</button>
+          </div> : <button onClick={() => {
+            window.location = window.location.href.includes('localhost')
+              ? 'http://localhost:8888/login'
+              : 'https://better-playlists-backend-sp.herokuapp.com/login'}
+          }
+          style={{ padding: '20px', 'font-size': '50px', 'margin-top': '20px' }}>Sign in with Spotify</button>
         }
       </div>
     );
